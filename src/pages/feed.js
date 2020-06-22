86469009762a299cd71bac0d32c090be543f5499
error-listen <danielsilva0.8892@hotmail.com>
2020-06-22 11:45:28 -0300

    Fix bug like

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -52,16 +52,6 @@ function Feed() {
         socket.on('post', new_post => {
             set_posts(posts.map(post => post._id === new_post._id ? new_post : post))
         })
-
-        const like = document.getElementsByClassName('like')
-        for (let i = 0; i < like.length; i++) {
-            like[i].innerHTML = '<svg aria-label="Dislike" fill="#ed4956" height="24" viewBox="0 0 48 48" width="24"><path clip-rule="evenodd" d="M35.3 35.6c-9.2 8.2-9.8 8.9-11.3 8.9s-2.1-.7-11.3-8.9C6.5 30.1.5 25.6.5 17.8.5 9.9 6.4 3.5 13.7 3.5 20.8 3.5 24 8.8 24 8.8s3.2-5.3 10.3-5.3c7.3 0 13.2 6.4 13.2 14.3 0 7.8-6.1 12.3-12.2 17.8z" fill-rule="evenodd"></path></svg>'
-        }
-
-        const dislike = document.getElementsByClassName('dislike')
-        for (let i = 0; i < dislike.length; i++) {
-            dislike[i].innerHTML = '<svg aria-label="Like" id="like" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clipRule="evenodd" d="M34.3 3.5C27.2 3.5 24 8.8 24 8.8s-3.2-5.3-10.3-5.3C6.4 3.5.5 9.9.5 17.8s6.1 12.4 12.2 17.8c9.2 8.2 9.8 8.9 11.3 8.9s2.1-.7 11.3-8.9c6.2-5.5 12.2-10 12.2-17.8 0-7.9-5.9-14.3-13.2-14.3zm-1 29.8c-5.4 4.8-8.3 7.5-9.3 8.1-1-.7-4.6-3.9-9.3-8.1-5.5-4.9-11.2-9-11.2-15.6 0-6.2 4.6-11.3 10.2-11.3 4.1 0 6.3 2 7.9 4.2 3.6 5.1 1.2 5.1 4.8 0 1.6-2.2 3.8-4.2 7.9-4.2 5.6 0 10.2 5.1 10.2 11.3 0 6.7-5.7 10.8-11.2 15.6z" fillRule="evenodd"></path></svg>'
-        }
     })
 
     function check_like(like) {
@@ -128,8 +118,10 @@ function Feed() {
                             </div>
                             <div className="article_content">
                                 {post.type === "image" ? <img src={post.file_url} width="100%" alt={!post.description ? "" : post.description} /> : <div><video width="100%" onClick={() => pause_video(post._id)} id={post._id}><source src={post.file_url}></source></video><span className="button" id={post._id} onClick={() => play_video(post._id)}><img alt="Play" src={play_image} /></span></div>}
-                                <button className={post.likes.filter(check_like).length > 0 ? "like" : "dislike"} onClick={() => like(post._id)}>
-                                    <svg aria-label="Like" fill="#262626" height="24" viewBox="0 0 48 48" width="24"> <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
+                                <button onClick={() => like(post._id)}>
+                                    {post.likes.filter(check_like).length > 0 ? 
+                                    <svg aria-label="Dislike" fill="#ed4956" height="24" viewBox="0 0 48 48" width="24"><path clipRule="evenodd" d="M35.3 35.6c-9.2 8.2-9.8 8.9-11.3 8.9s-2.1-.7-11.3-8.9C6.5 30.1.5 25.6.5 17.8.5 9.9 6.4 3.5 13.7 3.5 20.8 3.5 24 8.8 24 8.8s3.2-5.3 10.3-5.3c7.3 0 13.2 6.4 13.2 14.3 0 7.8-6.1 12.3-12.2 17.8z" fillRule="evenodd"></path></svg>  : 
+                                    <svg aria-label="Like" fill="#262626" height="24" viewBox="0 0 48 48" width="24"> <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>}
                                 </button>
                             </div>
                             <div className="article_footer">
@@ -145,7 +137,7 @@ function Feed() {
                                         ? post.likes.length > 5
                                             ? <span>Liked by <Link to={`/profile/${post.likes[0].username}`}><span className="bold_text">{post.likes[0].username}</span></Link> <span>and</span> {post.likes.length > 1
                                                 ? <span className="bold_text"> others {post.likes.length - 1} people</span>
-                                                : <span></span>}</span> : <span className="bold_text">{post.likes.length} likes</span>
+                                                : <span></span>}</span> : <span className="bold_text"><p>{post.likes.length} likes</p></span>
                                         : <span></span>}
 
                                 </div>
